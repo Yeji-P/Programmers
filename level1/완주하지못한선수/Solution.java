35fa510c97ebf3cc7d5345a20439210d9fe38985
Yeji <77283542+Yeji-P@users.noreply.github.com>
2021-05-01 04:43:47 +0900

    Create Solution.java

---
+++ level1/완주하지못한선수/Solution.java
@@ -0,0 +1,22 @@
+package level1.완주하지못한선수;
+
+import java.util.*;
+
+public class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        int i;
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for(i=0; i<completion.length; i++){
+            if(!participant[i].equals(completion[i])){
+                answer = participant[i];
+                return answer;
+            }
+        }
+        answer = participant[i];
+        return answer;
+    }
+}
